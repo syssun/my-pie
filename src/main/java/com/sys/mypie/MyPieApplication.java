d0759a00b41baf1f1bf85efb4ddf9cb8252a5e38
Your Name <you@example.com>
2020-12-14 22:55:33 +0800

    init1

---
+++ src/main/java/com/sys/mypie/MyPieApplication.java
@@ -0,0 +1,13 @@
+package com.sys.mypie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPieApplication.class, args);
+    }
+
+}
